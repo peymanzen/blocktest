fad32cc589c0bded010cf3277111ee46944fc5c3
peymanzen <panzalchi@gmail.com>
2024-03-23 10:25:39 +0400

    first commit

--- main.go
+++ main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
-	"github.com/peymanzen/blocktest/blockchain"
 	"fmt"
+	"strconv"
+
+	"github.com/peymanzen/blocktest/blockchain"
 )
 
 func main(){
-	chain := InitBlockChain()
+	chain := blockchain.InitBlockChain()
 	chain.AddBlock("FBAG")
 	chain.AddBlock("SBAG")
 	chain.AddBlock("TBAG")
 
-	for _, block := range chain.blocks{
+	for _, block := range chain.Blocks{
 		fmt.Printf("Prev hash: %x\n",block.PrevHash)
 		fmt.Printf("Data In Block: %s\n",block.Data)
 		fmt.Printf("Hash: %x\n",block.Hash)
+
+		pow := blockchain.newPoW(block)
+		fmt.Printf("PoW: %s\n",strconv.FormatBool(pow.Validate()))
+		fmt.Println()
 	}
 }
